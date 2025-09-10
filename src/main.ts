bab9be5a803c150f39c35f3c9ea1159dc41a5217
aadarsh-kawatra <thekawatra@gmail.com>
2025-09-10 21:04:05 +0530

    adding helmet package

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
+
   const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
